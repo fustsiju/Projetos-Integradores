0efd2032c6bb6d9bb4f72083bd8ff0dd51c23341
Fulvio Stefanine <62194735+fustsiju@users.noreply.github.com>
2023-06-28 10:45:50 -0300

    Add files via upload

---
+++ Site de Investimentos/script.js
@@ -0,0 +1,11 @@
+const api = {
+    key: "05239919",
+    base: "https://api.hgbrasil.com/finance?key=05239919",
+    lang: "pt_br",
+    units: "metric"
+}
+
+function darkM() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
